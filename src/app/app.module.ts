61cc9f40fd528d616b64b8d902abf37f04c0a547
AngelTravieso <angeltraviesoc@gmail.com>
2024-04-09 17:21:30 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CharactersComponent } from './components/characters/characters.component';
+
+@NgModule({
+  declarations: [AppComponent, CharactersComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}

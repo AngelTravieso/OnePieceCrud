d0794aba976482371a0a8fb3817696e37c2cf300
AngelTravieso <angeltraviesoc@gmail.com>
2024-04-10 14:16:02 -0400

    CHANGE - improve UI fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CharactersComponent } from './components/characters/characters.component';
+import { CharactersComponent } from './components/list-characters/characters.component';
 import { AddCharacterComponent } from './components/add-character/add-character.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';

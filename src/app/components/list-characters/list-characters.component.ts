bc79dc3fbe48caf64def5a491644682680c4e617
AngelTravieso <angeltraviesoc@gmail.com>
2024-04-10 16:41:33 -0400

    ADD - tooltip

--- src/app/components/list-characters/list-characters.component.ts
+++ src/app/components/list-characters/list-characters.component.ts
@@ -1,15 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 
 import { Character } from 'src/app/interfaces/character.interface';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-list-characters',
   templateUrl: './list-characters.component.html',
   styleUrls:['./list-characters.css'],
 })
-export class ListCharactersComponent {
+export class ListCharactersComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  }
 
   @Input()
   public characters: Character[] = [];
@@ -27,4 +35,8 @@ export class ListCharactersComponent {
   public drop(event: CdkDragDrop<Character[]>): void {
     moveItemInArray(this.characters, event.previousIndex, event.currentIndex);
   }
+
+
+
+  
 }
